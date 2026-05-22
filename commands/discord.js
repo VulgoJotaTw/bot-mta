860375c0bc1c3a66ee1defb202450d9a33e0eb52
JotaTw <jogoscraft81@gmail.com>
2026-05-22 20:01:26 -0300

    add comando discord

---
+++ commands/discord.js
@@ -0,0 +1,30 @@
+module.exports = {
+    name: 'discord',
+    execute(message) {
+        message.channel.send(`
+🌐 DISCORD OFICIAL DA CIDADE DE DEUS ROLEPLAY
+
+🔥 CIDADE DE DEUS ROLEPLAY 🔥
+
+🚀 O que oferecemos:
+✨ Sistemas exclusivos e diferenciados
+💼 Empregos exclusivos para você evoluir dentro da cidade
+🏢 Corporações ativas para crescimento profissional
+🔧 Mecânica livre para personalização e serviços automotivos
+🚑 SAMU disponível para atendimentos e RP realista
+🔫 Facções livres para quem busca dominar a cidade
+🌤️ Clima leve e agradável para um RP de qualidade
+🎮 Experiência imersiva para todos os tipos de jogadores
+
+📢 Fique por dentro de tudo!
+Acesse nossos chats no Discord e acompanhe todas as novidades, eventos e atualizações do servidor.
+
+⏳ Status do servidor: Em breve... prepare-se!
+
+💎 Venha viver uma experiência única na CIDADE DE DEUS ROLEPLAY e descubra um novo nível de Roleplay!
+
+🔗 Entre agora: 
+https://discord.gg/GFEB9KaGkC
+        `);
+    }
+};
